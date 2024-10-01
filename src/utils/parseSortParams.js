4471a12cba508cbb0c9eae6d7bcbe47467642c71
Alonka-Le <alyona.sholohowa120@gmail.com>
2024-10-01 14:45:52 +0200

    red

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,14 +1,14 @@
 import { SORT_ORDER } from '../constants/sortOrder.js';
 
 const parseSortParams = ({ sortBy, sortFields, sortOrder }) => {
-  const parseSortBy = sortFields.includes(sortBy) ? sortBy : '_id';
-  const parseSortOrder = SORT_ORDER.includes(sortOrder)
+  const parsedSortBy = sortFields.includes(sortBy) ? sortBy : '_id';
+  const parsedSortOrder = SORT_ORDER.includes(sortOrder)
     ? sortOrder
     : SORT_ORDER[0];
 
   return {
-    sortBy: parseSortBy,
-    sortOrde: parseSortOrder,
+    sortBy: parsedSortBy,
+    sortOrder: parsedSortOrder,
   };
 };
 
